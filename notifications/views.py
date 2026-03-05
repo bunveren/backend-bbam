53ccea21b206422338aaff4e5650459b1cba2ce7
aylinShn0 <aylin.kaan@hotmail.com>
2026-03-05 19:47:31 +0300

    added reminder controller logic

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from .models import WorkoutReminder
+from .serializers import WorkoutReminderSerializer
+from .services import NotificationService
 
 # Create your views here.
+class ReminderViewSet(viewsets.ModelViewSet):
+    serializer_class = WorkoutReminderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return WorkoutReminder.objects.filter(user=self.request.user)
+
+    def _trigger_sync(self, request):
+        sender_device_uuid = request.headers.get('X-Device-UUID')
+        if sender_device_uuid:
+            NotificationService.send_silent_sync_signal(request.user, sender_device_uuid)
+
+    def perform_create(self, serializer):
+        reminder = serializer.save(user=self.request.user)
+        self._trigger_sync(self.request)
+
+    def perform_update(self, serializer):
+        serializer.save()
+        self._trigger_sync(self.request)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        self._trigger_sync(self.request)
