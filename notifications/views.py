95898bdc27983dc33fa8d431883dab321bf3ef40
aylinShn0 <aylin.kaan@hotmail.com>
2026-04-18 18:11:40 +0300

    debug for sync

--- notifications/views.py
+++ notifications/views.py
@@ -19,7 +19,10 @@ class ReminderViewSet(viewsets.ModelViewSet):
     def _trigger_sync(self, request):
         sender_device_uuid = request.headers.get('X-Device-UUID')
         if sender_device_uuid:
+            print("sender uuid bulundu")
             NotificationService.send_silent_sync_signal(request.user, sender_device_uuid)
+        else:
+            print("DEBUG: Sinyal gönderilmedi çünkü X-Device-UUID header'ı eksik!")
 
     def perform_create(self, serializer):
         reminder = serializer.save(user=self.request.user)
